1f3bef8f03113b9ccc3848a1803acdd1b993a944
arthurjz <arthurjz@hotmail.fr>
2024-12-10 10:50:12 +0100

    dockerfile WIP

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,13 @@ import './index.css'
 import App from './App.tsx'
 import { store } from './store';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>,
-)
+// For server side rendering
+if (typeof window !== 'undefined') {
+    ReactDOM.createRoot(document.getElementById('root')!).render(
+        <StrictMode>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </StrictMode>,
+    )
+}
